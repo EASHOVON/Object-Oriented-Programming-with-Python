94b34aa9025c371078c99199c64a7a3e4517669e
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-09-30 17:01:41 +0600

    How write comment added

--- WeekOne_Mod_01/first.py
+++ WeekOne_Mod_01/first.py
@@ -1,9 +1,9 @@
 
-# For single line comment
+# For single line comment --> Ctrl+/
 
 print("Hello World"); # Line Comment
 
-""" And This is Called Multiline comment
+""" And This is Called Multiline comment --> Alt+Shift+A
 
 
     I am enjoying learning python
