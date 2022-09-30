ed1932f0348b88bdde8ecd4528c7c24afe1bd22a
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-09-30 21:15:51 +0600

    Practiced Some String operation

---
+++ WeekOne_Mod_01/string.py
@@ -0,0 +1,12 @@
+from __future__ import division
+
+
+countryName = "Bangladesh";
+
+divisionName = "Dhaka";
+
+myName = 'Shovon';
+
+location = divisionName + ', ' + countryName;
+print("My Name is",myName);
+print(location);
