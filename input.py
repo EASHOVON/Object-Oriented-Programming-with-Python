bbd6f3b6a56edf0f24525f6daeb24a622f4192c8
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-09-30 22:22:09 +0600

    Take Input from user and convert it to integer

---
+++ input.py
@@ -0,0 +1,12 @@
+""" name = input('Enter your Name please: ')
+age = input('Enter your age please: ')
+ """
+""" print(type(name),type(age)) """
+
+
+mathMark = input('Input your math mark: ')
+englishMark = input('Input your english number: ')
+
+totalMarks = int(mathMark)+int(englishMark)
+
+print(totalMarks);
