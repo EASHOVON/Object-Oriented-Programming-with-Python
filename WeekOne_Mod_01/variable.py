df6f52bec8d7aa5459507afd404a4ca2b6273644
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-09-30 18:46:25 +0600

    Variable in python

---
+++ WeekOne_Mod_01/variable.py
@@ -0,0 +1,9 @@
+mobilePrice = 10000;
+laptopPrice = 36000;
+carPrice = 1225000;
+
+myTotal = mobilePrice + laptopPrice + carPrice;
+print(myTotal);
+
+
+""" This is How the variable will Declared """
