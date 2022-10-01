121076c328350cca0ec979161e4bbeb93de212d9
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-01 11:52:57 +0600

    Tried some conditional Operation

---
+++ WeekOne_Mod_02/conditionnal.py
@@ -0,0 +1,9 @@
+biraniPrice = 100
+kababPrice = 75
+
+if biraniPrice < 150 :
+    print('Sultan a Khamu Fokir Homu!')
+elif kababPrice < 50:
+    print('Moja koira kabab Khamu!')
+else:
+    print('Kola Ruti e amar sob!')
