e81db58079342101f2b6640d2db1bb734cc70143
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-01 09:07:28 +0600

    Solved 1st HomeWork

---
+++ WeekOne_Mod_01/homeWork/problem_one.py
@@ -0,0 +1,7 @@
+numOne = input('Enter first Number: ')
+numTwo = input('Enter second Number: ')
+numThree = input('Enter third Number: ')
+
+sum = int(numOne) + int(numTwo) + int(numThree)
+
+print("The sum of three number",sum);
