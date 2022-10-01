082b029f9a1998003d32961c07f3d29382c00d5e
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-01 12:51:59 +0600

    Try some multiple conditioning operation

---
+++ WeekOne_Mod_02/MultipleCond.py
@@ -0,0 +1,17 @@
+is_rainy = True
+have_umbrela = True
+
+if is_rainy == True and have_umbrela == True:
+    print('Will go to out side')
+else:
+    print("Don't go outside please!")
+
+
+job = 'Police'
+salary = 30000
+
+if job == 'Police' or salary > 50000:
+    print('Tomar Sathei Biye dibo amar meyer!')
+
+else:
+    print('Tomar joggota nai!')
