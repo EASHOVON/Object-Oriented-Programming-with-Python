a82d44d4fecd418d010f2994693535bd6b9d3b92
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-01 23:43:15 +0600

    Made tip Calculator program

---
+++ Extra_Practice/tipCalculator.py
@@ -0,0 +1,11 @@
+food_amount = float(input('Enter food amount $: '))
+tip_percentage = float(input('Enter your tip percentage %: ')) / 100
+
+tip_amount = food_amount * tip_percentage
+
+total = food_amount + tip_amount
+print('----------------------------------')
+print(f'🥘 Food Amount: ${food_amount}')
+print(f'⚖️ Tip Amount: ${tip_amount}')
+print(f'💰 Total Amount: ${total}')
+print('----------------------------------')
