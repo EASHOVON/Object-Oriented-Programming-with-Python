fbd09be5d100e2e12b7e5704179169658bcd6833
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-01 09:46:39 +0600

    Solved HomeWork Four

---
+++ WeekOne_Mod_01/homeWork/problem_four.py
@@ -0,0 +1,7 @@
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+food = input('Enter food name: ')
+
+display = f'Hey, I am {name}. My age is {age}. I love to eat {food}.'
+
+print(display);
