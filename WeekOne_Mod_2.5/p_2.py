7bf2a6ae518b4a9f519eff59333a10235dce1a1f
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-02 07:31:42 +0600

    Solved Problem Two

---
+++ WeekOne_Mod_2.5/p_2.py
@@ -0,0 +1,12 @@
+numOne = int(input('Enter a Number: '))
+numTwo = int(input('Enter a Number: '))
+numThree = int(input('Enter a Number: '))
+if numOne < 0:
+    numOne *= -1
+if numTwo < 0:
+    numTwo *= -1
+if numThree < 0:
+    numThree *= -1
+print(numOne)
+print(numTwo)
+print(numThree)
