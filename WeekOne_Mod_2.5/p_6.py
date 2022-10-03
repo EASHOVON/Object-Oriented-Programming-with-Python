f69f908c1a2616e6f7ed7432e72650deefdc78fd
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-03 10:31:18 +0600

    Changed the pattern

--- WeekOne_Mod_2.5/p_6.py
+++ WeekOne_Mod_2.5/p_6.py
@@ -1,6 +1,6 @@
 for row in range(7):
-    for col in range(5):
-        if ((row==0 or row==3 or row==6) and (col>0 and col<4)) or (col==0 and(row>0 and row<3)) or (col==4 and (row>3 and row<6)):
+    for col in range(7):
+        if ((row==0 or row==3 or row==6) and (col>=0 and col<=6)) or (col==0 and(row>0 and row<3)) or (col==6 and (row>3 and row<6)):
             print('*',end='')
         else:
             print(end=' ')
