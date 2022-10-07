00e08c8d55f8cf65c47bd3a65821e0bd887fb56d
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-07 16:43:58 +0600

    Solved Printing fibonacci series

---
+++ WeekOne_Mod_3.5/prob_3.py
@@ -0,0 +1,25 @@
+""" 
+
+Display Fibonacci series up to 10 terms
+
+0  1  1  2  3  5  8  13  21  34
+
+"""
+
+def printFibonacci(length):
+
+    first = 0
+    second = 1
+    print(first,second,end=" ")
+    length -= 2
+
+    while length>0:
+        print(first+second,end=" ")
+
+        temp = second
+        second = first+second
+        first = temp
+        length -=1
+
+num = int(input("Enter a Num: "))
+printFibonacci(num)
