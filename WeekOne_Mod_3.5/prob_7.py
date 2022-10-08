5716d929504e025675a2533de6869c2494a9b9df
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-08 17:00:38 +0600

    Solved Another way of problem Seven

--- WeekOne_Mod_3.5/prob_7.py
+++ WeekOne_Mod_3.5/prob_7.py
@@ -2,9 +2,23 @@
 Input : 121
 Output : True
  """
-inp_number = input("Number: ")
+# inp_number = input("Number: ")
 
-if inp_number == inp_number[::-1]:
-    print("True")
+# if inp_number == inp_number[::-1]:
+#     print("True")
+# else:
+#     print("False")
+
+n = int(input("Enter a Number: "))
+temp = n
+rev = 0
+
+while n>0:
+    r = n % 10
+    rev = rev * 10 + r
+    n //= 10
+
+if temp==rev:
+    print(True)
 else:
-    print("False")
+    print(False)
