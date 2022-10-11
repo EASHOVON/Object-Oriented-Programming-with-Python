8b004c605886020ca63967bec1f80c5675151d6f
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-12 02:36:55 +0600

    Added class 4-5 code

---
+++ WeekTwo_Mod_4/4_5-import.py
@@ -0,0 +1,6 @@
+""" 4-5 Import Functions Written In Other Files
+We can access variables and functions declared in one file from another file by importing them. Two useful importing technique is given below - """
+
+from function import * # this will import everything from functions.py file
+from function import add, multiply # this will import add and multiply functions from a file named functions.py
+
