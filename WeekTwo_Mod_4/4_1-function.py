3f5f4bbdb14edf69c0582f424494b815f1c1cfcf
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-12 01:22:13 +0600

    Added 4.1 class code

---
+++ WeekTwo_Mod_4/4_1-function.py
@@ -0,0 +1,18 @@
+""" 4-1 Simple Introduction To Function In Python And Return.
+The def keyword is used to define a function and the return keyword is used to return something from the function. A few example of functions are given below - """
+
+def add(num1, num2):
+    return num1 + num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def substract(num1, num2):
+    return num1 - num2
+
+sum = add(35, 25)
+product = multiply(35, 25)
+difference = substract(product, sum)
+
+print(difference)
+
