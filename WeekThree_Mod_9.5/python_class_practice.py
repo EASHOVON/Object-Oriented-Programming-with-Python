8455e5fd48710300dd95bb52eb690066029e9ac0
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-20 20:51:41 +0600

    Created getter and setter with using @property of decoretor

--- WeekThree_Mod_9.5/python_class_practice.py
+++ WeekThree_Mod_9.5/python_class_practice.py
@@ -4,30 +4,41 @@ class Vehicle:
         self.name = name
         self.mileage = mileage
         self.capacity = capacity
-    # This is normal getter and setter method 
-    def getter(self):
-        print("--------------Vehicle Info----------------")
-        print(f'Vehicle Name: {self.name}, Vehicle Mileage: {self.mileage}, Vehicle Capacity: {self.capacity}')
-
-    def setter(self,name,mileage,capacity):
+    # # This is normal getter and setter method 
+    @property
+    def get_vehicle_name(self):
+        return self.name
+    @property
+    def set_vehicle_name(self,name):
         self.name = name
-        self.mileage = mileage
-        self.capacity = capacity
+
+    # Getter and Setter Method with Decorator property
+    @get_vehicle_name.getter
+    def get_vehicle_name(self):
+        return self.name
+    @set_vehicle_name.setter
+    def set_vehicle_name(self,name):
+        self.name = name
 
 class Bus(Vehicle):
     pass
 
-School_bus = Bus("School Volvo", 12, 50)
-
+# School_bus = Bus("School Volvo", 12, 50)
+# print(School_bus.getter_meth)
 
 # Number One Task to check type of school_bus object
-print(type(School_bus))
+# print(type(School_bus))
 
 # Number Two Task to check if the School_bus object is a instance of vehicle class
-print(isinstance(School_bus,Vehicle))
+# print(isinstance(School_bus,Vehicle))
 
 # Number Three Task --Define a property that must have the same value for every class instance (object)
 
 premo_car = Vehicle("Premeo",20,60)
-print(School_bus.manufactures)
-print(premo_car.manufactures)
+# print(School_bus.manufactures)
+# print(premo_car.manufactures)
+
+premo_car.set_vehicle_name = "Lambergeni"
+
+vehicleName = premo_car.get_vehicle_name
+print(vehicleName)
