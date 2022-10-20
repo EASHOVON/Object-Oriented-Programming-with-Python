4f66d3e1fe0ddc9d3a666cb55fd4c4aa9f8b46b6
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-20 11:49:13 +0600

    Assigning Functions to Variables

--- WeekThree_Mod_9/decorator-4.py
+++ WeekThree_Mod_9/decorator-4.py
@@ -1,19 +1,26 @@
-import math
-import time
+# import math
+# import time
 
-def timer(func):
-    def inner(*args,**kwargs):
-        print("Time Start")
-        start = time.time()
-        func(*args,**kwargs)
-        end = time.time()
-        print("Time End")
-        print(start-end)
-    return inner
+# def timer(func):
+#     def inner(*args,**kwargs):
+#         print("Time Start")
+#         start = time.time()
+#         func(*args,**kwargs)
+#         end = time.time()
+#         print("Time End")
+#         print(start-end)
+#     return inner
 
 
-@timer
-def factorial(n):
-    print(f'{n} factorial is: {math.factorial(n)}')
+# @timer
+# def factorial(n):
+#     print(f'{n} factorial is: {math.factorial(n)}')
 
-factorial(8)
+# factorial(8)
+
+""" Assigning Functions to Variables """
+def plus_one(number):
+    return number + 1
+
+add_one = plus_one
+add_one(5)
