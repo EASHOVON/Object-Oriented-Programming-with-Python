1da3341632d447659b25d55def52073d36e8827b
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-21 13:24:17 +0600

    Window Showing Done

--- WeekThree_Mod_10_Lab/main.py
+++ WeekThree_Mod_10_Lab/main.py
@@ -1,4 +1,8 @@
+""" My Camera Application
+Authro: MD. ASRAFUZZAMAN SUVON
+"""
 import sys
+from turtle import window_height, window_width
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap , QImage, QIcon
 import cv2
@@ -7,13 +11,30 @@ class Window(QWidget):
     """ Main app window """
     def __init__(self):
         super().__init__()
-
-        self.window_width = 600
+        #  Variable for App Window
+        self.window_width = 640
         self.window_height = 400
+
+        # Image Variables
+        self.img_width = 640
+        self.img_height = 400
+
+        # Set up the window
+        self.setWindowTitle("My Camera App")
+        self.setGeometry(0,0,self.window_width,self.window_height)
+        self.setFixedSize(self.window_width,self.window_height)
+
+
+        self.ui()
 
     def ui(self):
         """ Contains All Ui Things """
-        pass
+
+        # Image level
+        self.image_level = QLabel(self)
+        self.image_level.setGeometry(0,0,self.img_width,self.img_height)
+        
+        self.show()
 
     def update(self):
         """ update frames """
@@ -25,5 +46,14 @@ class Window(QWidget):
 
     def record(self):
         """ Record video """
+
+
+
+
+# Run
+app = QApplication(sys.argv)
+win = Window()
+sys.exit(app.exec_())
+
 
     
