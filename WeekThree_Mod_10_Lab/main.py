406d57a75cd465b0b5763e03a0417871f514b1ca
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-21 14:03:48 +0600

    Showing image done using camera

--- WeekThree_Mod_10_Lab/main.py
+++ WeekThree_Mod_10_Lab/main.py
@@ -2,9 +2,9 @@
 Authro: MD. ASRAFUZZAMAN SUVON
 """
 import sys
-from turtle import window_height, window_width
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap , QImage, QIcon
+from PyQt5.QtCore import QTimer
 import cv2
 
 class Window(QWidget):
@@ -24,6 +24,10 @@ class Window(QWidget):
         self.setGeometry(0,0,self.window_width,self.window_height)
         self.setFixedSize(self.window_width,self.window_height)
 
+        # Setup Timer
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+        # self.timer.timeout.connect(self.update)
 
         self.ui()
 
@@ -34,11 +38,21 @@ class Window(QWidget):
         self.image_level = QLabel(self)
         self.image_level.setGeometry(0,0,self.img_width,self.img_height)
         
+        if not self.timer.isActive():
+            self.cap = cv2.VideoCapture(0)
+            self.timer.start(20)
         self.show()
 
     def update(self):
         """ update frames """
-        pass
+        _, self.frame = self.cap.read()
+
+        frame = cv2.cvtColor(self.frame,cv2.COLOR_BGR2RGB)
+        height,width,channel = frame.shape
+        step = channel * width
+
+        q_frame = QImage(frame.data,width,height,step,QImage.Format_RGB888)
+        self.image_level.setPixmap(QPixmap.fromImage(q_frame))
 
     def save_img(self):
         """ Save image from camera """
