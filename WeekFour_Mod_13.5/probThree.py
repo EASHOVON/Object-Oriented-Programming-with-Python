53587766018ca3aa175052c66981d69f0fe279b3
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-26 23:50:34 +0600

    Solved Problem Three

---
+++ WeekFour_Mod_13.5/probThree.py
@@ -0,0 +1,5 @@
+""" 3. Identify which oops concept is used in the following scenario -
+" Group of 5 boys ,one boy never gives any contribution when the group goes for an outing, party or anything. Suddenly a one beautiful girl joins the same group and then after the same boy spends lots of money for the group. "
+ """
+
+print("Answer: This is runtime polymorphism")
