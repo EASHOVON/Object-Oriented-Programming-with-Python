f79bccb3d27680e54f29c5dae96a729b4e432e3e
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-26 23:56:07 +0600

    Solved Problem Five

---
+++ WeekFour_Mod_13.5/probFive.py
@@ -0,0 +1,8 @@
+""" 5. Identify the OOP concept - A person: Rahim - 
+*  in Home act is husband/son,
+* in Office acts as an Employer.
+* in Public Good Citizen.
+ """
+
+
+print("Answer: A person Rahim is the oop concept of Polymorphism.")
