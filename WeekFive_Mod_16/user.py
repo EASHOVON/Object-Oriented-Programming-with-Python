7e9386e0e178a6ad9ca16c13451366d7c4235b18
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-11-08 07:38:34 +0600

    Added Class Four code

--- WeekFive_Mod_16/user.py
+++ WeekFive_Mod_16/user.py
@@ -1,4 +1,7 @@
 import hashlib
+from brta import BRTA
+
+license_authority = BRTA()
 class User:
     def __init__(self,name,email,password) -> None:
         self.name = name
@@ -27,5 +30,53 @@ class User:
             return False
 
 
+class Rider(User):
+    def __init__(self, name, email, password,location, balance) -> None:
+        super().__init__(name, email, password)
+        self.location = location
+        self.balance = balance
+
+    def set_location(self,location):
+        self.location = location
+
+    def get_location(self):
+        return self.location
+
+    def request_trip(self,destination):
+        pass
+
+    def start_a_trip(self,fare):
+        self.balance -= fare
+
+class Driver(User):
+    def __init__(self, name, email, password,location,license) -> None:
+        super().__init__(name, email, password)
+        self.location = location
+        self.license = license
+        self.valid_driver = license_authority.validate_license(email,license)
+        self.earning = 0
+
+
+    def take_driving_test(self):
+        result = license_authority.take_driving_test(self.email)
+        if result == False:
+            print("Sorry you failed, Try again")
+        else:
+            self.license = result
+            self.valid_driver = True
+
+
+    def start_a_trip(self,destination,fare):
+        self.earning += fare
+        self.location = destination
+
 hero = User("Hero Alom","hero@alom.com","heroOhHero")
 User.log_in('hero@alom.com','heroOhHero')
+
+
+kuber = Driver("Kuber Majhi", "kuber@majhi.com","kopilajaishna", 54, 45)
+result = license_authority.validate_license(kuber.email,kuber.license)
+print(result)
+kuber.take_driving_test()
+result = license_authority.validate_license(kuber.email,kuber.license)
+print(result)
