b6cbe66c734f9a3012da95274ba2827ef98a1c23
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-11-17 15:53:23 +0600

    Added codes of module 16.5

---
+++ WeekFive_Mod_16/vehicle.py
@@ -0,0 +1,60 @@
+from abc import ABC,abstractmethod
+
+# Creating a Vehicle class
+class Vehicle(ABC):
+    speed = {
+        'car' : 30,
+        'bike' : 50,
+        'cng' : 15
+    }
+    def __init__(self,vehicle_type,licence_plate,rate,driver) -> None:
+        self.vehicle_type = vehicle_type
+        self.rate = rate
+        self.driver = driver
+        self.licence_plate = licence_plate
+        self.speed = self.speed[vehicle_type]
+
+    # Creating Abstract Method
+    @abstractmethod
+    def start_driving(self): 
+        pass
+
+    @abstractmethod
+    def trip_finished(self):
+        pass
+
+
+
+
+# Now I am creating Car Class
+class Car(Vehicle):
+    def __init__(self, vehicle_type, licence_plate, rate, driver) -> None:
+        super().__init__(vehicle_type, licence_plate, rate, driver)
+        
+    def start_driving(self):
+        print(self.vehicle_type,self.licence_plate,"Started")
+
+    def trip_finished(self):
+        print(self.vehicle_type,self.licence_plate,"Completed Trip")
+
+# Now I am creating Bike Class        
+class Bike(Vehicle):
+    def __init__(self, vehicle_type, licence_plate, rate, driver) -> None:
+        super().__init__(vehicle_type, licence_plate, rate, driver)
+
+    def start_driving(self):
+        print(self.vehicle_type,self.licence_plate,"Started")
+
+    def trip_finished(self):
+        print(self.vehicle_type,self.licence_plate,"Completed Trip")
+
+# Now I am creating Cng class
+class Cng(Vehicle):
+    def __init__(self, vehicle_type, licence_plate, rate, driver) -> None:
+        super().__init__(vehicle_type, licence_plate, rate, driver)
+
+    def start_driving(self):
+        print(self.vehicle_type,self.licence_plate,"Started")
+
+    def trip_finished(self):
+        print(self.vehicle_type,self.licence_plate,"Completed Trip")
